4c5810298c29f68634c06ab2ea9cf8555792816d
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-01 17:45:56 -0500

    [Add] Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0 <Success>
+ */
+int main(void)
+{
+	printf("123456789\n");
+
+	return (0);
+}
