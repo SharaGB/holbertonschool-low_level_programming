6a1d13cdbf7fb990e27ac4f100cb9d620b5e2ec1
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-01 18:53:16 -0500

    [Add] Task 9 whitout char

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Single-digit numbers
+ *
+ * Return: Always 0 <Success>
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
