8765675cf0a77793da2e5ea3e53218c22ac5d656
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-04 13:39:57 -0500

    [Add] Task 1 with prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 <Success>
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
