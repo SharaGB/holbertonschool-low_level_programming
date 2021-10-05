a1c8a2ed1e71b225bb92ae14e6b1932105a3a6b4
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-04 19:31:03 -0500

    [Add] Task 6 with integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - Absolute value
+ * @x: Check
+ *
+ * Return: n abdolute value of an integer
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = -1 * x;
+	}
+	return (x);
+}
