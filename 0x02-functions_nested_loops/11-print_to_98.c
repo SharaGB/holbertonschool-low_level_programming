67a8653431e3a445733ae5dd643526da6259f3c7
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-05 14:19:41 -0500

    [Add] Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers
+ * @n: Check
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%i, ", n);
+	}
+
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%i, ", n);
+	}
+	printf("98\n");
+}
