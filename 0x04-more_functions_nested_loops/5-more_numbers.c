8f863ed11eed9595d8f7cd3399c46bea34321498
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-07 18:20:16 -0500

    [Add] Task 5 priting 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers
+ *
+ * Return: Void
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
