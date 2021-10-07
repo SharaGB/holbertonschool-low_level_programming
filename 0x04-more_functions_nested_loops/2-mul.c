44e55309b88e6397555d52bc2df71c14e4c564bf
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-07 16:51:10 -0500

    [Add] Task 2 by multiplying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Two integers
+ *@a: firts integer
+ *@b: second integer
+ *
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
