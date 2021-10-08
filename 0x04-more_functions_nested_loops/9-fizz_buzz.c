41e0b01cbb468efee52cc6a58a55eac82d87044f
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-07 19:43:08 -0500

    [Add] Task 9 - FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x <= 100)
+		{
+			printf("%d ", x);
+		}
+		else
+		{
+			printf("%d", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
