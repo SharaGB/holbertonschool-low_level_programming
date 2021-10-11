109c87e19c21803ecb35a4f0851447fa5259a806
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-11 18:05:02 -0500

    [Add] Task 3 with sentence for

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - A string
+ * @str: Check the string
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	_putchar(str[x]);
+	_putchar('\n');
+}
