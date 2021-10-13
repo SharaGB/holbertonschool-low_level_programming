ed061dac7c14f78c96b71bcc0d98393a1a1e27d5
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-13 15:56:56 -0500

    [Add] Task 0 concatenating two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenates two strings
+ * @dest: check the string dest
+ * @src: check the sting src
+ *
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int l;
+	int a;
+
+	l = 0;
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	a = 0;
+	while (src[a] != '\0')
+	{
+		dest[l + a] = src[a];
+		a++;
+	}
+	src[a] = '\0';
+	return (dest);
+}
