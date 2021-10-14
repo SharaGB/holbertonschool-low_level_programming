4b027c219e96834a0ee7ce215e83dc4e4575ac79
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-14 18:24:38 -0500

    [Fix] Advanced task 8creating a string with rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *rot13 - Encodes a string using rot13
+ *@str: check the string
+ *
+ * Return: Always 0
+ */
+char *rot13(char *str)
+{
+	char *str1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *str2 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i;
+	int j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == str1[j])
+			{
+				str[i] = str2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
