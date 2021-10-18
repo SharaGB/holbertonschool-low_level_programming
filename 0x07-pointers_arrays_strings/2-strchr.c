ca839c9ba344b5dcb7f98258f0eb58de95d71bdf
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-18 14:00:24 -0500

    [Fix] return

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,10 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 		s++;
 	}
-	if (c == '\0')
-	{
-		return ('\0');
-	}
-	return (s);
+	return ('\0');
 }
