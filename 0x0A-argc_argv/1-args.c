87a2da4e94d3383992ea9515eabd293ddcd0c30b
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-22 15:49:22 -0500

    [Add] Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints the number of arguments
+ * @argc: Check argc
+ * @argv: Check of string
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
