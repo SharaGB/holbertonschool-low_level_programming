773e54d305aef0f8339a8ae26907a620e3b19c93
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-22 15:17:45 -0500

    [Add] Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints its name
+ * @argc: Check argc
+ * @argv: Check of string
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	if (argc == 1)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
