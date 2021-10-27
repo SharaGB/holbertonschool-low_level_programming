97f121de966963718d9253ba2c8f55ce1bdc2392
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-27 17:23:05 -0500

    [Add] Advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * main - The minimum number of coins
+ * @argc: Check arguments
+ * @argv: Check arrau arguments
+ * Return: 1 if error, else 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, pesos, count = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	pesos = atoi(argv[1]);
+	if (pesos < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 0; i < 5;)
+		{
+			if (pesos >= coins[i])
+			{
+				pesos -= coins[i];
+				count += 1;
+			}
+			if (pesos < coins[i])
+				i++;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
