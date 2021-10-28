0a27160d6b90926835a7fc1140d1deaa0fadbd79
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-28 17:47:59 -0500

    [Fix] malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,12 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	len2 = _strlen(s2);
-	str = (char *)malloc((len1 + n + 1) * sizeof(char));
+	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
-	{
-		free(str);
 		return (NULL);
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		str[i] = s1[i];
@@ -35,8 +33,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i + j] = s2[j];
 	}
-	if (n >= len2)
+	if (n < len2)
 	{
+		str = (char *)malloc((len1 + n + 1) * sizeof(char));
+		if (str == NULL)
+			return (NULL);
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			str[i] = s1[i];
+		}
 		for (j = 0; s2[j] != '\0'; j++)
 		{
 			str[i + j] = s2[j];
