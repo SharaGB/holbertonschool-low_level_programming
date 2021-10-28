1fbbd93764027575e7b4a3e727624618c17dcc3f
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-28 15:13:37 -0500

    [Fix] Check

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,6 +37,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= len2)
 	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			str[i] = s1[i];
+		}
 		for (j = 0; j < n; j++)
 		{
 			str[i + j] = s2[j];
