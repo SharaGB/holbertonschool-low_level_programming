7c9dfec775ad86f94f097b181b44a4904c3b445c
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-28 14:49:59 -0500

    [Fix] Check

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(char) * nmemb * size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
