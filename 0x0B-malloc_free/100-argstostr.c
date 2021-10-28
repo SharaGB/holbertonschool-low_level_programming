ba94289e92d65d530d44ad2e46a33b61ad54ca2f
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-27 22:56:45 -0500

    [Add] Advanced taks

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,78 @@
+#include "main.h"
+/**
+ * *argstostr - Concatenate two string.
+ * @ac: Check ac
+ * @av: Check arguments
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, len;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len = len + _strlen(av[i]) + 1;
+	}
+
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		_strncat(str, av[i]);
+		_strncat(str, "\n");
+	}
+	return (str);
+}
+
+/**
+ * _strlen - The length of a string
+ * @s: Check the string
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (s[leng] != '\0')
+		leng++;
+	return (leng);
+}
+
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: check the string dest
+ * @src: check the sting src
+ * @n: check
+ *
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
