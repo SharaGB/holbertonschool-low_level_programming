06022f0699d262af94703d50103ac85ecc1dfdd5
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-27 22:56:05 -0500

    [Fix] Header

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -7,3 +8,5 @@ char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char *_strncat(char *dest, char *src);
