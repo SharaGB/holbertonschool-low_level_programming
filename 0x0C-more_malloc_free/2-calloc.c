00863d29628208c073765c909c2b2f803a7d9989
SharaGB <shara.garcia.betancur@gmail.com>
2021-10-28 14:34:09 -0500

    [Add] Task 2 allocating memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_calloc - Allocates memory for an array
+ * @nmemb: Check number
+ * @size: Check size
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int n;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(nmemb * sizeof(char));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (n = 0; n > (nmemb * size); n++)
+	{
+		ptr[n] = 0;
+	}
+	return (ptr);
+}
