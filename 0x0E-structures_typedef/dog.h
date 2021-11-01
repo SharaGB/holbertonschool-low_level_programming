f09addac67a868b676496bd597c9b7a32dcdea01
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-01 17:44:18 -0500

    [Add] Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOH_H__
+
+/**
+ * struct dog - Check the code
+ * @name: Check name of dog
+ * @owner: Check owner
+ * @age: Check age
+ */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} my_dog;
+
+#endif
