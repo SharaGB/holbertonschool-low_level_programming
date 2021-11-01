04b25ade6bb59cb7b0206fbd0af5b570384853c6
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-01 18:08:23 -0500

    [Fix] double inclusion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,23 @@
-#ifndef _DOG_H_
-#define _DOH_H_
+#ifndef DOG_H
+#define DOG_H
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
  * struct dog - Check the code
  * @name: Check name of dog
- * @owner: Check owner of dog
  * @age: Check age of dog
+ * @owner: Check owner of dog
  */
 
 typedef struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-} my_dog;
+	char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
