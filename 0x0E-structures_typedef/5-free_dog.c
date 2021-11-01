9010ae859d015c5d88bed85b6f67f7dd4ffe114e
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-01 18:42:51 -0500

    [Add] Free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs
+ * @d: Check of pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+        free(d->age);
+	}
+    free(d);
+}
