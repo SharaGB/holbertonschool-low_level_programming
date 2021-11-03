67a2a060ebba4080123b25e10b1a9a1091fcada6
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-03 17:07:02 -0500

    [Add] Task 3 with simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+/**
+ *main - Contain your main function
+ *@argc: Check n of arguments
+ *@argv: Check
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int all_operations;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	all_operations = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", all_operations);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - Contain addition
+ * @a: Variable num1
+ * @b: Variable num2
+ * Return: Function
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Contain subtraction
+ * @a: Variable num1
+ * @b: Variable num2
+ * Return: Function
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: Variable num1
+ * @b: Variable num2
+ * Return: Function
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division
+ * @a: Variable num1
+ * @b: Variable num2
+ * Return: Function
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo
+ * @a: Variable num1
+ * @b: Variable num2
+ * Return: Function
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
