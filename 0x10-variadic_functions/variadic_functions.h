2e5bb3ab2f99deb3f1eed313853dd195d77665a3
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-05 13:15:09 -0500

    [Fix] Header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,20 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * format - List of types of arguments
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct format
+{
+	char *name;
+	void (*f)(va_list);
+} format_t;
+
+void format_char(va_list);
+void format_int(va_list);
+void format_float(va_list);
+void format_char_p(va_list);
+
 #endif
