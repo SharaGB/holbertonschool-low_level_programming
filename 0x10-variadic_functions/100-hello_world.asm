7266465a1243765bad8d7b0fc79561393b813db9
SharaGB <https://github.com/SharaGB>
2021-11-04 23:54:05 -0500

    [Fix] Requirement

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,7 +1,10 @@
 section .data
-    message db "Hello, world", 10
-	section .text
-	global main
+
+    message db "Hello, World", 10
+
+section .text
+
+global main
 main:
     mov rax, 1
     mov rdi, 1
