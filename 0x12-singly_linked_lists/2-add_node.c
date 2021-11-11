0cedb1dd10aebc4d7e4b2263c42193298e44a2d1
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-11 18:58:47 -0500

    Add task 2 adds a new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_node - Adds a new node
+ * @head: Main node
+ * @str: Check str
+ * Return: Adress of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	free(new);
+	return (new);
+}
+
+/**
+ * _strlen - The length of a string
+ * @s: Check the string
+ *
+ * Return: Always 0
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
