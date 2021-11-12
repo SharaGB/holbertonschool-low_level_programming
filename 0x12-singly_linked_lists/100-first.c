11d70db4fb3259d9460875bd22da0d72fd506d52
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-11 21:10:01 -0500

    Change %s

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 void first(void) __attribute__((constructor));
 void first(void)
 {
-	printf("%s", "You're beat! and yet, you must allow,\n");
-	printf("%s", "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
