9958b93a559d47d499495a8cbb78fbbf354de4d0
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-11 20:37:59 -0500

    Add a function that frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - Function that fress a list
+ * @head: Main node
+ * Return: Head
+ */
+void free_list(list_t *head)
+{
+    list_t *tmp, *n;
+
+    tmp = head;
+    while (tmp)
+    {
+        n = tmp->next;
+        free(tmp->str);
+        free(tmp);
+        tmp = n;
+    }
+}
