549e4027afd9bc7317e72bc59d925a98820fa184
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-11 21:07:08 -0500

    Add advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* first - Print Function
+* Return: Always 0
+*/
+void first(void) __attribute__((constructor));
+void first(void)
+{
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("%s", "bore my house upon my back!\n");
+}
