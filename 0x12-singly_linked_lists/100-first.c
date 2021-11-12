edc0eeec803e6e1ef3ccd9663469a0f4c6241651
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-11 21:08:49 -0500

    Fix printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@ void first(void) __attribute__((constructor));
 void first(void)
 {
 	printf("%s", "You're beat! and yet, you must allow,\n");
-	printf("%s", "bore my house upon my back!\n");
+	printf("%s", "I bore my house upon my back!\n");
 }
