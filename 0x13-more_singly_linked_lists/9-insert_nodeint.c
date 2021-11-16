d18ca6111eb2ae5a21e6278af8040a79c9ef85a2
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-16 11:30:51 -0500

    Add NULL at variable tmp

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	listint_t *tmp = *head;
+	listint_t *tmp = NULL;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (!new_node)
