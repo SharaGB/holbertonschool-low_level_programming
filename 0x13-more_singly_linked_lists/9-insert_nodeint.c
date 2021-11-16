cfe3825eb08fd86841c098069ef83503b0bb6fc6
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-16 12:59:03 -0500

    Fix code fail

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,16 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	tmp = *head;
 
-	while (count < idx - 1 && tmp != NULL)
+	while (count < idx - 1)
 	{
 		tmp = tmp->next;
-	}
-	if (tmp == NULL)
-	{
-		return (NULL);
-	}
-	count++;
 
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
+		count++;
+	}
 	new_node->next = tmp->next;
 	tmp->next = new_node;
 
