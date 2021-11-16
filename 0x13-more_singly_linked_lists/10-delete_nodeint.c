ecc31ac48788f5da936771dd3f00714e429388cd
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-16 16:30:14 -0500

    Add task 10 that delestes the node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Function that deletes the node
+ * @head: Pointer to the head of the list
+ * @index: Index of the node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *tmp = NULL;
+	listint_t *current = NULL;
+
+	if (!head || !*head)
+	{
+		return (-1);
+	}
+	tmp = *head;
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (count < index)
+	{
+		current = tmp;
+		tmp = tmp->next;
+
+		if (current == NULL)
+		{
+			return (-1);
+		}
+		count++;
+	}
+	current->next = tmp->next;
+	free(tmp);
+	return (1);
+}
