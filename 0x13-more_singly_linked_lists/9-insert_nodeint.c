18923ff732d7dc3eafa3e0259a6069e2a7ee2ef6
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-16 11:23:35 -0500

    Fix code fail

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if (!*head)
-	{
-		return (NULL);
-	}
 
 	if (idx == 0)
 	{
