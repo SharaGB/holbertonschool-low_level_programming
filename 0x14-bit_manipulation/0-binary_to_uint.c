0f100d95b51286d5ac78640326c6b412648a532b
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-18 15:09:38 -0500

    Add task 0 that converts a binary number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: Binary number
+ * Return: Converted number, or 0 if failed
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		dec <<= 1;
+
+		if (b[i] == '1')
+			dec += 1;
+		i++;
+	}
+	return (dec);
+}
