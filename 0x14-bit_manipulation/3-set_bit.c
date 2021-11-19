c49c32d056dcb1f09948f40ccf325c17a9e8a5ea
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-18 19:48:10 -0500

    Fix conditional

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
   * get_bit - Function that return the value of a bit
   * @n: Check number
   * @index: Check index
-  * Return: Value of the bit at index index or -1 if an error occured
+  * Return: 1 if it worked, or -1 if an error occurred
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
