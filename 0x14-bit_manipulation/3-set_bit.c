a48419e8b558b1d1f4226f8e4408a28f5f20b8ec
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-18 20:25:14 -0500

    Change conditional

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,11 +7,9 @@
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int x = 1;
-
 	if (index < 64)
 	{
-		*n = (*n & ~(1UL << index)) | (x << index);
+		*n = (1UL << index) | *n;
 		return (1);
 	}
 	return (-1);
