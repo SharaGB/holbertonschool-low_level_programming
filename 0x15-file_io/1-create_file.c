3ce2594f8f4c688f54046da3f05aea60b65af72c
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-23 11:11:25 -0500

    Fix code fail

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, sz_rw;
+	int sz_rw;
+	int fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (filename == NULL)
 	{
@@ -16,13 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fp = open(filename, O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
-		if (fp == -1)
-		{
-			return (-1);
-		}
 		return (1);
 	}
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	sz_rw = write(fp, text_content, strlen(text_content));
 	if (fp == -1 || sz_rw == -1)
 	{
