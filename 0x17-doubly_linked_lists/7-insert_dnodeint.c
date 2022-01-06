41b856bf2702484727d2d049f86a9e81d4088cbe
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-06 13:31:28 -0500

    Fix code adding conditional for prev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,14 +23,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = NULL;
 
 	if (h != NULL && idx == 0)
 	{
 		new_node->next = *h;
 		(*h)->prev = new_node;
 		*h = new_node;
-		return (new_node);
 	}
 	tmp = *h;
 	while (count < idx - 1)
