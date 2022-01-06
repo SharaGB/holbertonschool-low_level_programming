7b16a2669cb19c869bdc3883c67b1070ba2a66a2
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-06 12:07:11 -0500

    Fix conditional if

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,14 +14,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
-	*head = new_node;
 	if (!head)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (new_node);
 }
