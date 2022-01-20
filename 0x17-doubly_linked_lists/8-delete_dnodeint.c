a5129fe1e234b6f24b4ff1b1d581944b4e7eda5e
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-19 20:47:13 -0500

    Fix function that deletes the node at idx

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,12 +26,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (current->next == NULL)
 			return (-1);
-		if (!current->next && count == index)
-		{
-			current->prev->next = NULL;
-			free(current);
-			return (1);
-		}
 		if (count == index)
 		{
 			current->next->prev = current->prev;
