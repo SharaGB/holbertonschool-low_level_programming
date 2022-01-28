9f95ef9f1e8d1477962e2600b32a5c39dbff6455
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-28 17:53:58 -0500

    Trying fix function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	if (strlen(key) == 0 || !ht || !ht->array || !ht->size)
+	{
 		return (0);
+	}
 	index =  key_index((unsigned char *)key, ht->size);
 	new->key = strdup(key);
 	new->value = strdup(value);
