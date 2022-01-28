a31bdd9a63bf6db63cc82fb7ca671578cca780d3
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-28 18:24:47 -0500

    Fix code fail

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,14 +9,13 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	hash_node_t *element = NULL;
+	hash_node_t *element = ht->array[index];;
 
 	if (strlen(key) == 0 || !ht || !ht->array || !ht->size || !key)
 	{
 		return (NULL);
 	}
 	index =  key_index((unsigned char *)key, ht->size);
-	element = ht->array[index];
 	if (element != NULL)
 	{
 		while (strcmp(element->key, key) == 0)
