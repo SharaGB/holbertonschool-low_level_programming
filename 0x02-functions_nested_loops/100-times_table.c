47dedc18ae3e077d3e2680234b39aac423e98a74
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 12:30:27 -0500

    Add function that prints numbers mult

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * print_times_table - Function that prints the n times table, starting with 0.
+ * @n: Number
+ * Return: Number of the table.
+ */
+void print_times_table(int n)
+{
+	int i = 0;
+	int j = 0;
+	int mul = 0;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				mul = i * j;
+				if (j == 0)
+					_putchar(mul + '0');
+
+				else if (mul < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul + '0');
+				}
+				else if (mul < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mul / 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+				else if (mul >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((mul / 100) + '0');
+					_putchar(((mul / 100) % 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
