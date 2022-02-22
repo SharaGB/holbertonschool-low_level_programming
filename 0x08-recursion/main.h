c427fccf02897843a4b1c0b835a2bea5a570a898
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 16:39:49 -0500

    Add more prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -12,5 +13,6 @@ int _sqrt_recursion(int n);
 int prime_number(int n, int i);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
