d177550a30786e929272c720a885c9bc535fd481
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 12:43:27 -0500

    Fix print numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,12 +34,12 @@ void print_times_table(int n)
 					_putchar((mul / 10) + '0');
 					_putchar((mul % 10) + '0');
 				}
-				else if (mul >= 100)
+				else
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((mul / 100) + '0');
-					_putchar(((mul / 100) % 10) + '0');
+					_putchar(((mul / 10) % 10) + '0');
 					_putchar((mul % 10) + '0');
 				}
 			}
