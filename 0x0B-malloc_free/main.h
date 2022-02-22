960d344475548c07514c9c0fdd23bbe4edd75e15
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 17:19:05 -0500

    Add the directives for header

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,3 +12,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char *_strncat(char *dest, char *src);
+
+#endif /* _MAIN_H_ */
