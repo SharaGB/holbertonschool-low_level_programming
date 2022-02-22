7de4af43a20981f74963a6e2031dbd315ab481be
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 10:44:10 -0500

    [Add] Advanced taks 9 with Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -11,4 +10,3 @@ int main(void)
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
