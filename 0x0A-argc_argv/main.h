ecb2a6129f6f52fb70efa81d258f512a6e024658
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-22 17:00:25 -0500

    Add header

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int main(int argc, char **argv);
 int main(int argc, char **argv __attribute__((unused)));
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
+#endif /* _MAIN_H_ */
