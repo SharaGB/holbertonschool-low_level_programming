d38a631a1cfcf3d58687a7f2b7ab9fadd6ecf5cc
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-18 21:07:47 -0500

    Fix deletes the node at index index of a dlistint_t

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,42 +7,42 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int p = 0;
-	dlistint_t *tmp = NULL;
-	dlistint_t *saved_head = NULL;
+	unsigned int count = 0;
+	dlistint_t *current = *head;
 
-	if (*head == NULL)
-	{
+	if (!head || !*head)
 		return (-1);
-	}
-	saved_head = *head;
-	while (p < index && *head != NULL)
+	if (index == 0)
 	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
+		*head = current->next;
+		if (current->next != NULL)
 		{
-			tmp->prev = NULL;
+			current->next->prev = NULL;
 		}
+		free(current);
+		return (1);
 	}
-	else
+	while (current)
 	{
-		(*head)->prev->next = (*head)->next;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
+		if (!current->next)
+		{
+			current->prev->next = NULL;
+			free(current);
+			return (1);
+		}
+		if (count == index)
+		{
+			current->next->prev = current->prev;
+			current->prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		count += 1;
+		current = current->next;
 	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
 	return (1);
 }
