0534a25f83bd7f90941e7e56741a270606c9d3da
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-11 17:16:16 -0500

    Fix variable defined

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,15 +8,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
 	if (array != NULL)
 	{
-		for (i = 0; i < size; i++)
+		while (i < size)
 		{
-			printf("Value checked array[%lu] =[%d]\n", i, array[i]);
+			printf("Value checked array[%d] =[%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
+			i++;
 		}
 	}
 	return (-1);
